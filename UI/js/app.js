9923d9a22ddee7823c03508797839fbcee8ba320
andela-eshaibu <emmanuel.shaibu@andela.com>
2017-02-17 17:39:15 +0100

    UI update

--- UI/js/app.js
+++ UI/js/app.js
@@ -2,4 +2,17 @@ const app = angular.module('myApp', []);
 
 app.controller('MainController', ['$scope', ($scope) => {
     $scope.message = 'We are set to build our inverted index project';
+    $scope.fileList = ['File 1', 'File2'];
+    $scope.table1IsVisible = false;
+    $scope.table2IsVisible = false;
+    $scope.setActiveFile = (file) => {
+        $scope.activeFile = file;
+        $scope.table1IsVisible = true;
+        $scope.table2IsVisible = false;
+    };
+    $scope.showSearchTable = () => {
+        $scope.activeFile = false;
+        $scope.table1IsVisible = false;
+        $scope.table2IsVisible = true;
+    };
 }]);
