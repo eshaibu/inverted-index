5b6fdca37cddbe5643ef59120eea4026ce19763a
andela-eshaibu <emmanuel.shaibu@andela.com>
2017-03-02 12:45:22 +0100

    remove from bundle js

--- spec/inverted-index-test.js
+++
@@ -1,7 +0,0 @@
-// const books = require('./books');
-
-describe('check book length', () => {
-    it('book should not be empty', () => {
-        expect(books.length > 0).toBeTruthy();
-    });
-});
